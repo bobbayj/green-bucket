80fee2ca62d4426d920fc1366667f3348087d031
jin-and-tonic <35100300+jindustries@users.noreply.github.com>
2019-06-22 20:11:41 +1000

    [refactor] calculate CGT within new class

--- portfoliotracker.py
+++ portfoliotracker.py
@@ -1,21 +1,25 @@
 #%%
 import csv
 import os
+import numpy as np
 import pandas as pd
 import asxdata
 import mydatabase
+import datetime
 
 # Global vars
 tx_filename = 'private_bob_asx.csv'
 tx_folder = 'portfolio'
 tx_path = os.path.join(tx_folder,tx_filename)
 dbms = mydatabase.MyDatabase(mydatabase.SQLITE,dbname='mydb.sqlite')
-brokerage = {'<1': 10, '1-5':20, '>5':50}
 
 def txCsvToDf(txType='Contract'):
     '''Note that this currently only works for CSVs exported from Commsec
     Commsec does not inlcude dividends in transactions. The user will
     need to manually integrate dividends into the transactions CSV
+    --------------
+    txType: Contract, Payment, Receipt, or Dividend (default = 'Contract')
+            Type of transaction to read and return in Dataframe
     '''
     df = pd.read_csv(tx_path, parse_dates=True ,index_col=['Date'], dayfirst=True)
     df[['Trade','Qty','Code','drop','Price']] = df['Detail'].str.split(' ',expand=True)
@@ -30,7 +34,7 @@ def txCsvToDf(txType='Contract'):
     return df
 
 def get_portfolio_holdings():
-    '''Creates the portfolio from transactions df
+    '''Creates the portfolio's holdings from transactions df
     '''
     dfTrades = txCsvToDf()
         
@@ -55,7 +59,7 @@ def get_portfolio_holdings():
 
     return dfHoldings
 
-def get_portfolio_value(holdings, plot=False):
+def get_portfolio_value(holdings, plot):
     value = pd.DataFrame(index=holdings.index, columns=holdings.columns)
     for code in holdings.columns:
         query = 'SELECT * from "historical" WHERE code = "' + code + '"'
@@ -74,114 +78,131 @@ def get_portfolio_value(holdings, plot=False):
 
     return value, ax
 
-def get_CGT(fincyr=None):
+def get_CGT(code, tradedf, FY):
     '''
-    fincyr: FY string as YYYY (default = None). Creates Australian FY range. Otherwises, computes all capital gains
+    code: ASX code (required)
+    tradedf: DataFrame of trades
+    FY: FincYear string as YYYY (default = current calendar year).
     '''
-    # Vars
-    df = txCsvToDf()
-    # dfCGT = pd.DataFrame(index = df.index, columns = df.columns)
-    if fincyr:
-        fincyrStart = f'{fincyr-1}-07-01'
-        fincyrEnd = f'{fincyr}-06-30'
+    FYStart = f'{FY-1}-07-01' 
+    FYEnd = f'{FY}-06-30'
 
-    # for code in df.Code.unique():
-    #     dfTemp = df.loc[df['Code'] == code].sort_index(ascending=False)
-    #     buys = dfTemp.loc[dfTemp.Trade =='B','Qty'].todict()
-    #     sells = dfTemp.loc[dfTemp.Trade =='B','Qty'].todict()
-    #     dfCGT = dfTemp
-    #     break
+    tradedf = tradedf.loc[tradedf['Code'] == code]
 
+    # Convert sells in selected FY to a list of dicts (records)
+    dfTemp = tradedf[FYStart:FYEnd].reset_index()
+    sells = dfTemp.loc[dfTemp.Trade=='S'].assign(
+        **dfTemp.loc[dfTemp.Trade=='S'].select_dtypes(['datetime']).astype(str).to_dict('list')
+    ).to_dict('records')
 
-    return df
+    # Convert all buys to a list of dicts (records) in reverse order (for LIFO method)
+    # Buys also up to last sale date
+    if sells:
+        lastSellDate = sells[-1]['Date']
+        dfTemp = tradedf[:lastSellDate].sort_index(ascending=False).reset_index()
+    else:
+        dfTemp = tradedf.sort_index(ascending=False).reset_index()
+    buys = dfTemp.loc[dfTemp.Trade=='B'].assign(
+        **dfTemp.loc[dfTemp.Trade=='B'].select_dtypes(['datetime']).astype(str).to_dict('list')
+    ).to_dict('records')
 
-# ------ Main ------
+    capgains = []
+    buy_count = 0
+    for sell in sells:
+        while sell['Qty'] > 0:
+            if sell['Qty'] == buys[buy_count]['Qty']:
+                capgain = sell['Qty'] * (sell['Price'] - buys[buy_count]['Price'])
+                timedelta = pd.to_datetime(sell['Date']) - pd.to_datetime(buys[buy_count]['Date'])
+                if timedelta > pd.Timedelta('365 days'):
+                    CGTaxable = capgain/2
+                else:
+                    CGTaxable = capgain
+                sell['Qty'] = 0
 
-# dfHoldings = get_portfolio_holdings()
-# value, ax = get_portfolio_value(dfHoldings)
-FY = 2018
-FYStart = f'{FY-1}-07-01' 
-FYEnd = f'{FY}-06-30'
+            elif sell['Qty'] < buys[buy_count]['Qty']:
+                capgain = sell['Qty'] * (sell['Price'] - buys[buy_count]['Price'])
+                timedelta = pd.to_datetime(sell['Date']) - pd.to_datetime(buys[buy_count]['Date'])
+                if timedelta > pd.Timedelta('365 days'):
+                    CGTaxable = capgain/2
+                else:
+                    CGTaxable = capgain
+                buys[buy_count]['Qty'] = buys[buy_count]['Qty'] - sell['Qty']
+                sell['Qty'] = 0
 
-df = get_CGT()
-code = 'NAN'
-
-df = df.loc[df['Code'] == code]
-
-# Convert all buys to a list of dicts (records) in reverse order (LIFO method)
-dfTemp = df.sort_index(ascending=False).reset_index()
-buys = dfTemp.loc[dfTemp.Trade=='B'].assign(
-    **dfTemp.loc[dfTemp.Trade=='B'].select_dtypes(['datetime']).astype(str).to_dict('list')
-).to_dict('records')
-
-# Convert sells in selected FY to a list of dicts (records)
-dfTemp = df[FYStart:FYEnd].reset_index()
-sells = dfTemp.loc[dfTemp.Trade=='S'].assign(
-    **dfTemp.loc[dfTemp.Trade=='S'].select_dtypes(['datetime']).astype(str).to_dict('list')
-).to_dict('records')
-
-#%%
-capgains = []
-buy_count = 0
-for sell in sells:
-    sell_qty = sell['Qty'] #Don't overwrite the data
-    while sell_qty > 0:
-        buy_qty = buys[buy_count]['Qty'] #Don't overwrite the data
-        if sell_qty == buy_qty:
-            capgain = sell_qty * (sell['Price'] - buys[buy_count]['Price'])
-            timedelta = pd.to_datetime(sell['Date']) - pd.to_datetime(buys[buy_count]['Date'])
-            if timedelta > pd.Timedelta('365 days'):
-                CGTaxable = capgain/2
-            else:
-                CGTaxable = capgain
+            elif sell['Qty'] > buys[buy_count]['Qty']:
+                capgain = buys[buy_count]['Qty'] * (sell['Price'] - buys[buy_count]['Price'])
+                timedelta = pd.to_datetime(sell['Date']) - pd.to_datetime(buys[buy_count]['Date'])
+                if timedelta > pd.Timedelta('365 days'):
+                    CGTaxable = capgain/2
+                else:
+                    CGTaxable = capgain
+                sell['Qty'] = sell['Qty'] - buys[buy_count]['Qty']
+                
+            
             capgains.append({
-                'gain/loss': capgain,
-                'sale_price': sell['Price'],
-                'buy_price': buys[buy_count]['Price'],
-                'sale_date': sell['Date'],
-                'buy_date': buys[buy_count]['Date'],
+                'profit': capgain,
+                'sellPrice': sell['Price'],
+                'buyPrice': buys[buy_count]['Price'],
+                'sellDate': sell['Date'],
+                'buyDate': buys[buy_count]['Date'],
                 'timedelta': timedelta,
-                'CGTaxable': CGTaxable
+                'CGTaxable': CGTaxable,
             })
-            sell_qty = 0
-        elif sell_qty < buy_qty:
-            buy_qty = buy_qty - sell_qty
-            capgain = sell_qty * (sell['Price'] - buys[buy_count]['Price'])
-            timedelta = pd.to_datetime(sell['Date']) - pd.to_datetime(buys[buy_count]['Date'])
-            if timedelta > pd.Timedelta('365 days'):
-                CGTaxable = capgain/2
-            else:
-                CGTaxable = capgain
-            capgains.append({
-                'gain/loss': capgain,
-                'sale_price': sell['Price'],
-                'buy_price': buys[buy_count]['Price'],
-                'sale_date': sell['Date'],
-                'buy_date': buys[buy_count]['Date'],
-                'timedelta': timedelta,
-                'CGTaxable': CGTaxable
-            })
-            sell_qty = 0
-        elif sell_qty > buy_qty:
-            sell_qty = sell_qty - buy_qty
-            capgain = buy_qty * (sell['Price'] - buys[buy_count]['Price'])
-            timedelta = pd.to_datetime(sell['Date']) - pd.to_datetime(buys[buy_count]['Date'])
-            if timedelta > pd.Timedelta('365 days'):
-                CGTaxable = capgain/2
-            else:
-                CGTaxable = capgain
-            capgains.append({
-                'gain/loss': capgain,
-                'sale_price': sell['Price'],
-                'buy_price': buys[buy_count]['Price'],
-                'sale_date': sell['Date'],
-                'buy_date': buys[buy_count]['Date'],
-                'timedelta': timedelta,
-                'CGTaxable': CGTaxable
-            })
-        buy_count = buy_count + 1
 
-#%%
-buys
+            if not sell['Qty'] < buys[buy_count]['Qty']:
+                buy_count = buy_count + 1
+
+    return capgains
+
+def brokerageCalc(qty,price,broker='Commsec'):
+    ''' Default is Commsec brokerage rates
+    brokerage = {'0-1': 10, '1-10':19.95, '10-25':29.95, '>25': 0.12%}
+    --------
+    qty = Quantity of securities traded
+    price = Price of securities traded
+    '''
+    value = qty*price
+    if value < 1001:
+        brokerage = 10
+    elif value > 1001 and value < 10001:
+        brokerage = 19.95
+    elif value > 10001 and value < 25001:
+        brokerage = 29.95
+    elif value > 25001:
+        brokerage = 0.0012 * value
+    
+    return brokerage
+
+class portfolioAnalysis:
+    def __init__(self):
+        self.tradedf = txCsvToDf()
+    
+    def CGTaxable(self,FY=datetime.datetime.now().year):
+        '''Calculates CGTaxable for the given financial year (defaults to current calendar year)
+        '''
+        codes = self.tradedf.Code.unique()
+        CGTevents = {}
+        for code in codes:
+            CGTevents.update({code: get_CGT(code,self.tradedf,FY)})
+
+        # Calculate total CGTaxable for FY
+        totalCGTaxable = 0
+        for key, events in CGTevents.items():
+            if key != 'totalCGTaxable':
+                if len(events) > 0:
+                    for event in events:
+                        totalCGTaxable = totalCGTaxable + event['CGTaxable']
+        return totalCGTaxable
+
+    def holdings(self):
+        '''Returns portfolio holdings as Dataframe
+        '''
+        return get_portfolio_holdings()
+    
+    def value(self, plot=False):
+        '''Returns portfolio value as Dataframe
+        Able to plot total portfolio value
+        '''
+        return get_portfolio_value(self.holdings(),plot)
 
 #%%
